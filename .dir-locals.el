1741febee9153b7412b390c7e07e36d08441c419
mpanarin <mykhailopanarin@gmail.com>
2019-08-13 17:57:32 +0300

    [SPACEMACS] add example of function evaluation to dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,6 @@
 ((lsp-mode .
            ((lsp-pyls-plugins-pylint-args . ["--rcfile=./.pylintrc"])))
  (python-mode .
-              ((fill-column . 80))))
+              ((fill-column . 80)))
+ (web-mode .
+           ((eval . (web-mode-set-engine "django")))))
