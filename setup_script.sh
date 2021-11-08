c7f185061692d4fc24ee377b6b97a029a70d11a9
mpanarin <mykhailopanarin@gmail.com>
2021-11-09 00:48:06 +0200

    wip 4

--- setup_script.sh
+++ setup_script.sh
@@ -27,20 +27,15 @@ sudo pacman -S --noconfirm python2-pip \
      unzip \
      docker \
      docker-compose \
-     zsh-autosuggestions \
-     zsh-syntax-highlighting \
      kitty \
      glu \
      mesa \
      wxgtk2 \
      libpng \
-     erlang \
-     elixir \
      xsel \
      flameshot \
      otf-fira-code \
      brave \
-     yapf \
      xclip \
      bat
 
@@ -48,7 +43,7 @@ sudo pacman -S --noconfirm python2-pip \
 sudo pacman -R yakuake --noconfirm
 
 # Additional packages from AUR
-yay -S emacs-git telegram-desktop spotify-dev peco slack-desktop fpp-git lets-git
+yay -S emacs-git telegram-desktop spotify peco asdf-vm
 
 # add user to docker group and enable the service
 sudo systemctl enable docker
@@ -59,14 +54,7 @@ sudo usermod -a -G docker $USER
 sudo pip install --upgrade pip
 sudo pip2 install --upgrade pip
 
-# install some python packages
-# TODO: finish this pip install --user ical2orgpy
-
-# install poetry
-curl -sSL https://raw.githubusercontent.com/sdispater/poetry/master/get-poetry.py | python3
-
 # setup asdf
-git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.7.7
 asdf plugin-add erlang elixir
 
 # install spacemacs
@@ -75,9 +63,6 @@ git clone https://github.com/syl20bnr/spacemacs ~/.emacs.d -b develop
 # symlink the spacemacs config
 ln -fs ~/.dotfiles/.spacemacs ~/.spacemacs
 
-# install all the fonts
-bash ~/.dotfiles/install_fonts.sh
-
 # install tmp - tmux plugin manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
@@ -85,8 +70,7 @@ git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 ln -fs ~/.dotfiles/.tmux.conf ~/.tmux.conf
 
 # install tmuxinator
-gem install tmuxinator -v 1.1.4
-sudo ln -s ~/.gem/ruby/2.7.0/bin/tmuxinator /usr/bin/tmuxinator
+gem install tmuxinator
 
 # install oh-my-zsh
 curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh > install.sh
@@ -94,8 +78,13 @@ sh install.sh --unattended
 rm install.sh
 
 # install spaceship zsh theme
-git clone https://github.com/denysdovhan/spaceship-prompt.git ~/.oh-my-zsh/custom/themes/spaceship-prompt
-ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
+git clone https://github.com/denysdovhan/spaceship-prompt.git $ZSH/custom/themes/spaceship-prompt
+ln -s ~/.oh-my-zsh/custom/themes/spaceship-prompt/spaceship.zsh-theme $ZSH/custom/themes/spaceship.zsh-theme
+# install plugins
+git clone https://github.com/jeffreytse/zsh-vi-mode $ZSH/custom/plugins/zsh-vi-mode
+git clone https://github.com/zsh-users/zsh-autosuggestions $ZSH/custom/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/gusaiani/elixir-oh-my-zsh.git $ZSH/custom/plugins/elixir
 
 # symlink zshrc
 ln -fs ~/.dotfiles/.zshrc ~/.zshrc
@@ -110,16 +99,8 @@ git clone https://github.com/lifepillar/vim-solarized8.git ~/.vim/pack/themes/op
 # add kitty config folder
 mkdir -p ~/.config/kitty
 
-# add several zsh plugins
-git clone https://github.com/gusaiani/elixir-oh-my-zsh.git ~/.oh-my-zsh/custom/plugins/elixir
-
 # set-up vim Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-
-# add yapf configuration
-mkdir -p ~/.config/yapf
-ln -sf ~/.dotfiles/style ~/.config/yapf/style
-
 
 # symlink remaining files
 ln -fs ~/.dotfiles/.gitconfig ~/.gitconfig
@@ -141,4 +122,3 @@ ln -fs ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 ln -fs ~/.dotfiles/snazzy.conf ~/.config/kitty/snazzy.conf
 ln -fs ~/.dotfiles/.ripgreprc ~/.ripgreprc
 ln -fs ~/.dotfiles/tmux_get_startup_command ~/tmux_get_startup_command
-# TODO: finish this ln -fs ~/.dotfiles/cal2org_sync.zsh ~/cal2org_sync.zsh
